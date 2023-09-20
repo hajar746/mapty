d7efabca3aea11b33217cb66ad4c7f1c1dbbb0fb
hajar746 <132266440+hajar746@users.noreply.github.com>
2023-09-20 13:27:59 +0300

    Add leaflet map, setup marker and popup design

--- script.js
+++ script.js
@@ -10,3 +10,45 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+// getting coordinates from geolocation
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      const { latitude, longitude } = position.coords;
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
+
+      //   map from leaflet
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 11);
+
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      //   event listener to put marker wherever you click
+      map.on('click', function (mapEvent) {
+        console.log(mapEvent);
+        // getting coordinates from mapEvent object
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+              //popup options
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('Workout') //popup text
+          .openPopup();
+      });
+    },
+    function () {
+      alert('Could not get your location');
+    }
+  );
